c32897958606bf4ae4ce436be58fc073c166ef40
smk <sm.kim@invant.co>
2025-09-13 11:25:44 +0900

    [정리] EnumPopup Attribute 속성, 에디터 클래스를 한 스크립트로 통일

--- Assets/Scripts/Utils/DVEnumPopupAttribute.cs
+++ Assets/Scripts/Utils/DVEnumPopupAttribute.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
 public class DVEnumPopupAttribute : PropertyAttribute
@@ -8,3 +11,28 @@ public class DVEnumPopupAttribute : PropertyAttribute
 
     public DVEnumPopupAttribute(Type enumType) => EnumType = enumType;
 }
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(DVEnumPopupAttribute))]
+public class DVEnumPopupAttributeEditor : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        DVEnumPopupAttribute dvEnum = (DVEnumPopupAttribute)attribute;
+
+        // 여기서 타입 검사
+        if (property.propertyType != SerializedPropertyType.String
+            || (dvEnum.EnumType == null || !dvEnum.EnumType.IsEnum))
+        {
+            EditorGUI.PropertyField(position, property, label);
+            return;
+        }
+
+        string[] enumNames = System.Enum.GetNames(dvEnum.EnumType);
+        int currentIndex = Mathf.Max(0, System.Array.IndexOf(enumNames, property.stringValue));
+        int selectedIndex = EditorGUI.Popup(position, label.text, currentIndex, enumNames);
+
+        property.stringValue = enumNames[selectedIndex];
+    }
+}
+#endif
