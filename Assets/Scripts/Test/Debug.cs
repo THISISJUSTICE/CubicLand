1c0c3d5d7a1a48ae6afefc9a90925849c1d7489e
smk <sm.kim@invant.co>
2026-03-28 16:50:02 +0900

    [리팩토링] Test에서만 Debug Log를 적용할 Test Assembly 추가

---
+++ Assets/Scripts/Test/Debug.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace CustomTIJI
+{
+    public static class Debug
+    {
+        const string SYMBOL = "TEST_DEBUG";
+
+        [Conditional(SYMBOL)]
+        public static void Log(string message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+
+        [Conditional(SYMBOL)]
+        public static void LogWarning(string message)
+        {
+            UnityEngine.Debug.LogWarning(message);
+        }
+
+        [Conditional(SYMBOL)]
+        public static void LogError(string message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+
+        [Conditional(SYMBOL)]
+        public static void LogFormat(string message)
+        {
+            UnityEngine.Debug.LogFormat(message);
+        }
+
+        [Conditional(SYMBOL)]
+        public static void DrawRay(Vector3 start, Vector3 dir, Color? color = null, float duration = 0f)
+        {
+            if (color == null)
+                color = Color.white;
+            UnityEngine.Debug.DrawRay(start, dir, (Color)color, duration);
+        }
+
+        [Conditional(SYMBOL)]
+        public static void DrawLine(Vector3 start, Vector3 dir, Color? color = null, float duration = 0f)
+        {
+            if (color == null)
+                color = Color.white;
+            UnityEngine.Debug.DrawLine(start, dir, (Color)color, duration);
+        }
+    }
+}
