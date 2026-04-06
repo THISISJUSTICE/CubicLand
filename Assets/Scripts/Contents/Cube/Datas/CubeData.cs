68d545efc5a44481e9c761d1fdf7773b147d351e
smk <sm.kim@invant.co>
2026-04-06 20:49:33 +0900

    [변경] CubeData를 class로 변경
    - IsCore 옵션 제거

--- Assets/Scripts/Contents/Cube/Datas/CubeData.cs
+++ Assets/Scripts/Contents/Cube/Datas/CubeData.cs
@@ -7,24 +7,22 @@ namespace CustomTIJI.CubicLand.Cube
         public StatusPoint StatusPoint { get; private set; }
         public StatusValue StatusValue { get; private set; }
         public Vector3Int ShapePoisition { get; private set; }
-        public bool IsCore { get; private set; }
         public bool IsAttackMode { get; internal set; }
         public Color Color { get; private set; }
 
-        public CubeData(StatusPoint statusPoint, Vector3Int shapePosition, bool isCore, Color color)
-        { 
+        public CubeData(StatusPoint statusPoint, Vector3Int shapePosition, Color color)
+        {
             StatusPoint = statusPoint;
             StatusValue = new StatusValue();
             StatusValue.Initialize(statusPoint);
             ShapePoisition = shapePosition;
-            IsCore = isCore;
             IsAttackMode = false;
             Color = color;
         }
 
         public CubeData Copy()
         {
-            return new CubeData(StatusPoint, ShapePoisition, IsCore, Color);
+            return new CubeData(StatusPoint, ShapePoisition, Color);
         }
 
         public CubeData MakeChildData(Vector3Int shapePosition)
@@ -32,11 +30,11 @@ namespace CustomTIJI.CubicLand.Cube
             Color color = Color * CubeConfig.COLOR_CHILD_RATE;
             color.Clamp(color, Color.white);
 
-            return new CubeData(StatusPoint.MakeChildStatus(), shapePosition, false, color);
+            return new CubeData(StatusPoint.MakeChildStatus(), shapePosition, color);
         }
 
         public void EnhanceStatus(StatusPoint statusPoint)
-        { 
+        {
             StatusPoint = statusPoint;
             StatusValue.EnhanceStatus(statusPoint);
         }
