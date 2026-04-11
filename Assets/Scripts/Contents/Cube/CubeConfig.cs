f4a2525dea50fdbb6afd17c0a923d3270db47655
smk <sm.kim@invant.co>
2026-04-11 13:45:57 +0900

    [정리] Cube 관련 상수명을 더 명확하게 명명 및 정리

--- Assets/Scripts/Contents/Cube/CubeConfig.cs
+++ Assets/Scripts/Contents/Cube/CubeConfig.cs
@@ -7,25 +7,21 @@ namespace CustomTIJI.CubicLand.Cube
         public static readonly Vector3Int CORE_POSITION = Vector3Int.zero;
 
         public const float CUBE_BASE_LENGHT = 1f;
-
         public const float ONE_CUBE_MASS = 10f;
-
         public const float MASS_RATE = 0.2f;
 
-        public const float IMPULSE_DAMAGE_RATE = 0.3f;
+        public const float DAMAGE_SCALING_FACTOR = 6f;
+        public const float ADDITIONAL_DAMAGE_IMPULSE_DIVISOR = 0.3f;
+        public const float ARMOR_EXPONENTIAL_SCALE = 50f;
 
         public const float MAX_CUBE_NOMALIZE_TIME = 0.2f;
 
         public const float COLOR_CHILD_RATE = 1.1f;
 
-        public const float DAMAGE_IMPULSE_RATE = 6f;
-
         public const float RESTITUTION = 0.5f;
 
         public const float WEIGHT_EXPONENT = 0.2f;
         public const float SPEED_EXPONENT = 0.3f;
-
-        public const float DAMAGE_ARMOR_RATE = 50f;
 
         public static class Status
         {
