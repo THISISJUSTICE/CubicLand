bfc6a1e6d6796db7a22b06de2251fb3e1016e0ca
smk <sm.kim@invant.co>
2026-04-18 15:12:19 +0900

    [변경] CubeFactory에서는 CubeObject만 반환하도록 변경

--- Assets/Scripts/Contents/Cube/Interfaces/ICubeFactory.cs
+++ Assets/Scripts/Contents/Cube/Interfaces/ICubeFactory.cs
@@ -2,8 +2,7 @@ namespace CustomTIJI.CubicLand.Cube
 {
     public interface ICubeFactory
     {
-        public CubeObject CreateObjectCube(CubeData cubeData);
-        public ObstacleCube CreateObstacleCube(CubeData cubeData);
+        public CubeObject CreateCube(CubeData cubeData);
         public void DestoryCube(CubeObject golemcube);
     }
 }
