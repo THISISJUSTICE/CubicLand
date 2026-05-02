bb6fc6c2f14507f9aecbcee1d7c0d9f565373d8d
smk <sm.kim@invant.co>
2026-05-02 12:22:45 +0900

    [변경] CubeMotionAdjuster에 MoveVelocity 추가

--- Assets/Scripts/Contents/Cube/Interfaces/ICubeMotionAdjuster.cs
+++ Assets/Scripts/Contents/Cube/Interfaces/ICubeMotionAdjuster.cs
@@ -4,6 +4,7 @@ namespace CustomTIJI.CubicLand.Cube
 {
     public interface ICubeMotionAdjuster
     {
+        public Vector3 MoveVelocity { get; }
         public void ApplyKnockback(Vector3 impulse);
         public void NormalizeTransform();
     }
