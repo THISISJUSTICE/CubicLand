e0bce79ed13b83e2d4769e1f8ab6f7215ea6cbdc
smk <sm.kim@invant.co>
2026-08-01 13:26:55 +0900

    [변경] T value가 있는 OperationResult 타입 추가

--- Assets/Scripts/Commar/OperationResult.cs
+++ Assets/Scripts/Commar/OperationResult.cs
@@ -11,7 +11,38 @@ namespace Commar
             ErrorMessage = errorMessage;
         }
 
-        public static OperationResult GetSuccessedResult() => new OperationResult(true, null);
-        public static OperationResult GetFailedResult(string errorMessage) => new OperationResult(false, errorMessage);
+        public static OperationResult GetSuccessResult()
+        {
+            return new OperationResult(true, string.Empty);
+        }
+
+        public static OperationResult<T> GetSuccessResult<T>(T value)
+        {
+            return new OperationResult<T>(true, string.Empty, value);
+        }
+
+        public static OperationResult GetFailedResult(string errorMessage)
+        {
+            return new OperationResult(false, errorMessage);
+        }
+
+        public static OperationResult<T> GetFailedResult<T>(string errorMessage)
+        {
+            return new OperationResult<T>(false, errorMessage, default);
+        }
+    }
+
+    public struct OperationResult<T>
+    {
+        public bool IsSuccess { get; private set; }
+        public string ErrorMessage { get; private set; }
+        public T Value { get; private set; }
+
+        public OperationResult(bool isSuccess, string errorMessage, T value)
+        {
+            IsSuccess = isSuccess;
+            ErrorMessage = errorMessage;
+            Value = value;
+        }
     }
 }
