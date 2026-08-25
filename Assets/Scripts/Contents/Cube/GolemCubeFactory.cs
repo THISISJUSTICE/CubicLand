9755c64d602d828e2bbe70d24f3f9941dc2d21df
smk <sm.kim@invant.co>
2026-08-25 15:02:42 +0900

    [오류수정] GolemCube Load 키 변경

--- Assets/Scripts/Contents/Cube/GolemCubeFactory.cs
+++ Assets/Scripts/Contents/Cube/GolemCubeFactory.cs
@@ -2,7 +2,7 @@ namespace Commar.CubicLand.Cube
 {
     public class GolemCubeFactory : ObjectCubeFactory
     {
-        protected override string LoadKey => "GolemCube";
+        protected override string LoadKey => "ObjectCube";
 
         public GolemCubeFactory(IObjectPool objectPool, IAsyncAssetLoader assetLoader, ICubeSpawnEffect spawnEffect)
             : base(objectPool, assetLoader, spawnEffect)
