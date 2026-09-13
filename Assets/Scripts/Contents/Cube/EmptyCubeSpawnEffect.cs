954de81dc1c372bf1501894bcdbb5507e14ca443
smk <sm.kim@invant.co>
2026-09-13 15:21:44 +0900

    [신규] 임시 CubeSpawnEffect 구성

---
+++ Assets/Scripts/Contents/Cube/EmptyCubeSpawnEffect.cs
@@ -0,0 +1,8 @@
+namespace Commar.CubicLand.Cube
+{
+    // 임시 플레이 테스트용: 생성 연출을 생략합니다.
+    public class EmptyCubeSpawnEffect : ICubeSpawnEffect
+    {
+        public void Play(CubeObject cube) { }
+    }
+}
